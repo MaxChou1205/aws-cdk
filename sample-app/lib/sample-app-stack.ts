c50c361e2751f74c1c567a7bdf70e077832abf4c
MaxChou1205 <v2asault@yahoo.com.tw>
2023-02-24 23:19:02 +0800

    add pipeline

--- sample-app/lib/sample-app-stack.ts
+++ sample-app/lib/sample-app-stack.ts
@@ -4,6 +4,17 @@ import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Bucket, BucketEncryption } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
+import { Artifact } from "aws-cdk-lib/aws-codepipeline";
+import {
+  CodeBuildAction,
+  GitHubSourceAction
+} from "aws-cdk-lib/aws-codepipeline-actions";
+import { SecretValue } from "aws-cdk-lib";
+import {
+  BuildSpec,
+  LinuxBuildImage,
+  PipelineProject
+} from "aws-cdk-lib/aws-codebuild";
 
 export class SampleAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -17,6 +28,49 @@ export class SampleAppStack extends cdk.Stack {
       runtime: Runtime.NODEJS_18_X,
       entry: path.join(__dirname, "..", "api", "get-photos", "index.ts"),
       handler: "getPhotos"
+    });
+
+    const pipeline = new cdk.aws_codepipeline.Pipeline(
+      this,
+      "MyFirstPipeline",
+      {
+        pipelineName: "sampleAppPipeline"
+      }
+    );
+
+    const sourceOutput = new Artifact("SourceOutput");
+    pipeline.addStage({
+      stageName: "Source",
+      actions: [
+        new GitHubSourceAction({
+          actionName: "Pipeline_Source",
+          owner: "MaxChou1205",
+          repo: "aws-cdk/sample-app",
+          branch: "master",
+          oauthToken: SecretValue.secretsManager("github-pipeline-token"),
+          output: sourceOutput
+        })
+      ]
+    });
+
+    const cdkBuildOutput = new Artifact("CdkBuildOutput");
+    pipeline.addStage({
+      stageName: "Build",
+      actions: [
+        new CodeBuildAction({
+          actionName: "CDK_Build",
+          input: sourceOutput,
+          outputs: [cdkBuildOutput],
+          project: new PipelineProject(this, "CdkBuildProject", {
+            environment: {
+              buildImage: LinuxBuildImage.STANDARD_6_0
+            },
+            buildSpec: BuildSpec.fromSourceFilename(
+              "build-specs/cdk-build-spec.yml"
+            )
+          })
+        })
+      ]
     });
 
     new cdk.CfnOutput(this, "MySimpleAppBucketNameExport", {
